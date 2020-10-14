99a69425c677cbb71fea03eaadaf4fd4a0ed0e14
wanghongliang001 <1192668984@qq.com>
2020-10-14 10:33:52 +0800

    ProcessDefinitionController

--- src/main/java/com/imooc/activiti7_workflow/controller/ProcessDefinitionController.java
+++ src/main/java/com/imooc/activiti7_workflow/controller/ProcessDefinitionController.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,7 +29,7 @@ public class ProcessDefinitionController {
     // 添加流程定义通过上传bpmn
     @PostMapping(value = "/uploadStreamAndDeployment")
     public AjaxResponse uploadStreamAndDeployment(@RequestParam("processFile") MultipartFile multipartFile,
-                                                  @RequestParam("deploymentName") String deploymentName){
+                                                  @RequestParam("deploymentName") String deploymentName) {
 
         try {
             // 获取上传文件名
@@ -38,7 +40,7 @@ public class ProcessDefinitionController {
             InputStream fileInputStream = multipartFile.getInputStream();
 
             Deployment deployment = null;
-            if(extension.equals("zip")){
+            if (extension.equals("zip")) {
                 ZipInputStream zip = new ZipInputStream(fileInputStream);
                 deployment = repositoryService.createDeployment() // 初始化部署
                         .addZipInputStream(zip)
@@ -57,7 +59,7 @@ public class ProcessDefinitionController {
                     GlobalConfig.ResponseCode.SUCCESS.getDesc(),
                     deployment.getId() + ";" + fileName
             );
-        } catch (Exception e){
+        } catch (Exception e) {
             return AjaxResponse.AjaxData(
                     GlobalConfig.ResponseCode.ERROR.getCode(),
                     "部署流程失败",
@@ -70,11 +72,11 @@ public class ProcessDefinitionController {
     // 添加流程定义在线提交BPMN的XML
     @PostMapping(value = "/addDeploymentByString")
     public AjaxResponse addDeploymentByString(@RequestParam("stringBPMN") String stringBPMN,
-                                                  @RequestParam("deploymentName") String deploymentName){
+                                              @RequestParam("deploymentName") String deploymentName) {
 
         try {
             Deployment deployment = repositoryService.createDeployment()
-                    .addString("CreateWithBPMNJS.bpmn",stringBPMN)
+                    .addString("CreateWithBPMNJS.bpmn", stringBPMN)
                     .name(deploymentName)
                     .deploy();
 
@@ -83,7 +85,7 @@ public class ProcessDefinitionController {
                     GlobalConfig.ResponseCode.SUCCESS.getDesc(),
                     deployment.getId()
             );
-        } catch (Exception e){
+        } catch (Exception e) {
             return AjaxResponse.AjaxData(
                     GlobalConfig.ResponseCode.ERROR.getCode(),
                     "string部署流程失败",
@@ -94,7 +96,7 @@ public class ProcessDefinitionController {
 
     // 获取流程定义列表
     @GetMapping(value = "/getDefinitions")
-    public AjaxResponse getDefinitions(){
+    public AjaxResponse getDefinitions() {
 
 
         List<HashMap<String, Object>> listMap = new ArrayList<HashMap<String, Object>>();
@@ -127,7 +129,7 @@ public class ProcessDefinitionController {
                     GlobalConfig.ResponseCode.SUCCESS.getDesc(),
                     listMap     //list.toString()
             );
-        } catch (Exception e){
+        } catch (Exception e) {
             return AjaxResponse.AjaxData(
                     GlobalConfig.ResponseCode.ERROR.getCode(),
                     "获取流程定义失败",
@@ -138,8 +140,82 @@ public class ProcessDefinitionController {
 
 
     // 获取流程定义XML
+    @GetMapping(value = "/getDefinitionXML")
+    public void getDefinitionXML(HttpServletResponse response,
+                                 @RequestParam("deploymentID") String deploymentID,
+                                 @RequestParam("resourceName") String resourceName) {
+
+        try {
+
+            InputStream inputStream = repositoryService.getResourceAsStream(deploymentID, resourceName);
+            int count = inputStream.available();
+            byte[] bytes = new byte[count];
+            response.setContentType("text/xml");
+            OutputStream outputStream = response.getOutputStream();
+            while (inputStream.read(bytes) != -1) {
+                outputStream.write(bytes);
+            }
+            inputStream.close();
+
+        } catch (Exception e) {
+            e.toString();
+        }
+    }
 
     // 获取流程部署列表
+    @GetMapping(value = "/getDeployments")
+    public AjaxResponse getDeployments() {
+
+        List<HashMap<String, Object>> listMap = new ArrayList<HashMap<String, Object>>();
+
+        try {
+            List<Deployment> list = repositoryService.createDeploymentQuery()
+                    .list();
+
+            for (Deployment dep : list) {
+                HashMap<String, Object> hashMap = new HashMap<>();
+
+                hashMap.put("Id:", dep.getId());
+                hashMap.put("Name:", dep.getName());
+                hashMap.put("DeploymentTime:", dep.getDeploymentTime());
+
+                listMap.add(hashMap);
+            }
+
+            return AjaxResponse.AjaxData(
+                    GlobalConfig.ResponseCode.SUCCESS.getCode(),
+                    GlobalConfig.ResponseCode.SUCCESS.getDesc(),
+                    listMap     //list.toString()
+            );
+        } catch (Exception e) {
+            return AjaxResponse.AjaxData(
+                    GlobalConfig.ResponseCode.ERROR.getCode(),
+                    "获取流程部署列表失败",
+                    e.toString()
+            );
+        }
+    }
+
     // 删除流程定义
+    @GetMapping(value = "/delDefinition")
+    public AjaxResponse delDefinition(@RequestParam("pdID") String pdID) {
+
+        try {
+            repositoryService.deleteDeployment(pdID, true); //所有定义部署,历史都删掉
+
+            return AjaxResponse.AjaxData(
+                    GlobalConfig.ResponseCode.SUCCESS.getCode(),
+                    GlobalConfig.ResponseCode.SUCCESS.getDesc(),
+                    null
+            );
+        } catch (Exception e) {
+            return AjaxResponse.AjaxData(
+                    GlobalConfig.ResponseCode.ERROR.getCode(),
+                    "删除流程定义失败",
+                    e.toString()
+            );
+        }
+    }
+
 
 }
