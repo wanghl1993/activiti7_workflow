2cb5f7652e1e770543ca091b6fccbe7d5fdc4c55
wanghongliang001 <1192668984@qq.com>
2020-10-20 15:04:59 +0800

    1.linux 部署, 2.layui-mini, 3.war包

--- src/main/java/com/imooc/activiti7_workflow/Activiti7WorkflowApplication.java
+++ src/main/java/com/imooc/activiti7_workflow/Activiti7WorkflowApplication.java
@@ -2,12 +2,20 @@ package com.imooc.activiti7_workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class Activiti7WorkflowApplication {
+@SpringBootApplication(scanBasePackages = {"com.imooc.activiti7_workflow"})
+public class Activiti7WorkflowApplication extends SpringBootServletInitializer {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(Activiti7WorkflowApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Activiti7WorkflowApplication.class);
+    }
 }
--- src/main/java/com/imooc/activiti7_workflow/util/GlobalConfig.java
+++ src/main/java/com/imooc/activiti7_workflow/util/GlobalConfig.java
@@ -5,8 +5,11 @@ public class GlobalConfig {
 
     public static final Boolean Test = true;    //不需要调用登录接口
 
+    // 公司电脑windows
+    public static final String BPMN_PathMapping = "file:E:\\gitee\\activiti7_workflow2\\src\\main\\resources\\resources\\bpmn";
+
     // windows发布路径
-    public static final String BPMN_PathMapping = "file:F:\\gitee\\activiti7_workflow\\src\\main\\resources\\resources\\bpmn\\";
+//    public static final String BPMN_PathMapping = "file:F:\\gitee\\activiti7_workflow\\src\\main\\resources\\resources\\bpmn\\";
 
     // linux发布路径
     //public static final String BPMN_PathMapping = "/root/Activiti";
