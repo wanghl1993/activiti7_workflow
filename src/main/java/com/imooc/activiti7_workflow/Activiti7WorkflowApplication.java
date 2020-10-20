d0eee6b1614a2199137afa3ac110627f6df95d70
wanghongliang001 <1192668984@qq.com>
2020-10-20 18:10:10 +0800

    10-3 ActivitiWeb项目 扩展

--- src/main/java/com/imooc/activiti7_workflow/Activiti7WorkflowApplication.java
+++ src/main/java/com/imooc/activiti7_workflow/Activiti7WorkflowApplication.java
@@ -5,17 +5,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = {"com.imooc.activiti7_workflow"})
-public class Activiti7WorkflowApplication extends SpringBootServletInitializer {
-
-
+@SpringBootApplication
+public class Activiti7WorkflowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Activiti7WorkflowApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Activiti7WorkflowApplication.class);
-    }
 }
+
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication(scanBasePackages = {"com.imooc.activiti7_workflow"})
+//public class Activiti7WorkflowApplication extends SpringBootServletInitializer {
+//
+//
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Activiti7WorkflowApplication.class, args);
+//    }
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(Activiti7WorkflowApplication.class);
+//    }
+//}
---
+++ src/main/java/com/imooc/activiti7_workflow/listener/TkListener.java
@@ -0,0 +1,11 @@
+package com.imooc.activiti7_workflow.listener;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+public class TkListener implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        System.out.println("执行人: " + delegateTask.getAssignee());
+    }
+}
